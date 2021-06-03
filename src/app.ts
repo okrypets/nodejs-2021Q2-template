658650194636ceb37beb8b5daa0d2eddb5d6b918
Olga Studzenichnik <okrypets@gmail.com>
2021-06-03 13:03:14 +0300

    feat: implement listener for uncaughtException and unhandledRejection

--- src/app.ts
+++ src/app.ts
@@ -8,17 +8,24 @@ import path from 'path';
 import YAML from 'yamljs';
 import fs from 'fs';
 
+process.on('uncaughtException', (err: Error, origin: string) => {
+  fs.writeSync(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` +
+    `Exception origin: ${origin}`
+  );
+});
+
+process.on('unhandledRejection', (reason: Error, promise: Promise<Error>):void => {
+  console.error('Unhandled Rejection at: Promise', promise, 'reason:', reason.message);
+});
+
+Promise.reject(Error('Oops!'));
+
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
-
-process.on('uncaughtException', (err, origin) => {
-  fs.writeSync(
-    process.stderr.fd,
-    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
-  );
-});
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
