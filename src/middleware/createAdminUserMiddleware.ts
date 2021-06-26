db29f03ad86c115ba9bd3d591143b7631c63d3bc
Olga Studzenichnik <okrypets@gmail.com>
2021-06-26 11:03:30 +0300

    refactor: remove legacy console.log

--- src/middleware/createAdminUserMiddleware.ts
+++ src/middleware/createAdminUserMiddleware.ts
@@ -5,8 +5,7 @@ import userRepo from "../resources/users/user.repository";
 export const createAdminUserMiddleware = async (_req: Request, _res: Response, next: NextFunction): Promise<void> => {
     const adminUser = await userRepo.getByLogin("admin");
     if (!adminUser) {
-        const createdAdmin = await userRepo.create({id: v4(), name: "admin", login: "admin", password: "admin"});
-        console.log(createdAdmin)
+        await userRepo.create({id: v4(), name: "admin", login: "admin", password: "admin"});
     }
     next()
 }
